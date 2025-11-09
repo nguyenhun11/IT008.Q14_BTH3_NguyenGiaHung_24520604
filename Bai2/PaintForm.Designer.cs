7cf8922f109b933be95b103f2799da6051b36f0e
Gia Hưng Nguyễn <giahungcantho11@gmail.com>
2025-11-09 22:00:49 +0700

    di ve

---
+++ Bai2/PaintForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Bai2
+{
+    partial class PaintForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.contentPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.Color.LightGreen;
+            this.title.Dock = System.Windows.Forms.DockStyle.Top;
+            this.title.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(0, 0);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(733, 75);
+            this.title.TabIndex = 0;
+            this.title.Text = "Paint";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // contentPanel
+            // 
+            this.contentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.contentPanel.Location = new System.Drawing.Point(0, 75);
+            this.contentPanel.Name = "contentPanel";
+            this.contentPanel.Size = new System.Drawing.Size(733, 391);
+            this.contentPanel.TabIndex = 1;
+            // 
+            // PaintForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(733, 466);
+            this.Controls.Add(this.contentPanel);
+            this.Controls.Add(this.title);
+            this.Name = "PaintForm";
+            this.Text = "Paint";
+            this.Click += new System.EventHandler(this.PaintForm_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintForm_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Panel contentPanel;
+    }
+}
+
---
+++ Bai2/PaintForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai2
+{
+    public partial class PaintForm : Form
+    {
+        public PaintForm()
+        {
+            InitializeComponent();
+            this.CreateGraphics();
+        }
+
+        private void DrawPaint()
+        {
+            Random rnd = new Random();
+            Graphics draw = contentPanel.CreateGraphics();
+            int x = rnd.Next(10, Size.Width - 10);
+            int y = rnd.Next(10, Size.Height - 10);
+            Font font = new Font(new FontFamily("Segoe UI"), rnd.Next(8, 30), FontStyle.Bold, GraphicsUnit.Pixel);
+            SolidBrush solidBrush = new SolidBrush(Color.FromArgb(255, rnd.Next(255), rnd.Next(255), rnd.Next(255)));
+            draw.DrawString("Paint Event", font, solidBrush, new PointF(x, y));
+        }
+
+        private void PaintForm_Paint(object sender, PaintEventArgs e)
+        {
+            DrawPaint();
+        }
+
+        private void PaintForm_Click(object sender, EventArgs e)
+        {
+            DrawPaint();           
+        }
+    }
+}
