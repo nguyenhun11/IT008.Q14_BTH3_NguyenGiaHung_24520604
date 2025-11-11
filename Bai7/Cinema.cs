a1f537f923d5d986078c2c42481f8627774031da
Gia Hưng Nguyễn <giahungcantho11@gmail.com>
2025-11-12 01:05:02 +0700

    Finish 7

--- Bai7/Cinema.cs
+++ Bai7/Cinema.cs
@@ -12,6 +12,7 @@ namespace Bai7
 {
     public partial class Cinema : Form
     {
+        #region Dữ liệu chỗ ngồi
         enum SeatStatus
         {
             Unsold, Sold
@@ -20,17 +21,19 @@ namespace Bai7
         {
             public SeatStatus status { get; private set; }
             public bool isChoosing { get; private set; }
-            Button button;
+            public Button button { get; private set; }
+            public int price;
             Color emptyColor = Color.Gainsboro;
             Color chooseColor = Color.PaleTurquoise;
             Color soldColor = Color.PaleGoldenrod;
 
-            public Seat(Button button)
+            public Seat(Button button, int price = 0)
             {
                 this.button = button;
                 button.BackColor = emptyColor;
                 status = SeatStatus.Unsold;
                 isChoosing = false;
+                this.price = price;
             }
 
             public bool Choose()
@@ -59,20 +62,33 @@ namespace Bai7
 
             public int Order()
             {
-                if (!isChoosing)
+                if (!isChoosing || status == SeatStatus.Sold)
                 {
                     return 0;
                 }
+                isChoosing = false;
+                status = SeatStatus.Sold;
                 button.BackColor = soldColor;
-                return 0;
+                return price;
+            }
+
+            public void Cancel()
+            {
+                if (status == SeatStatus.Unsold && isChoosing)
+                {
+                    isChoosing = false;
+                    button.BackColor = emptyColor;
+                }
             }
         }
-        static List<Seat> seats;//Dữ liệu chung
+        List<Seat> seats;
+        #endregion
 
         public Cinema()
         {
             InitializeComponent();
-            panelThanhTien.Visible = false;
+            HideTotalCost();
+            // Khởi tạo và gán các button vào dữ liệu chỗ ngồi
             seats = new List<Seat>();
             seats.Add(null);
             seats.Add(new Seat(button1));
@@ -90,23 +106,64 @@ namespace Bai7
             seats.Add(new Seat(button13));
             seats.Add(new Seat(button14));
             seats.Add(new Seat(button15));
+            for(int i = 1; i <= 15; i++)
+            {
+                if(i <= 5)
+                {
+                    seats[i].price = 5000;
+                }
+                else if (i <= 10)
+                {
+                    seats[i].price = 6500;
+                }
+                else
+                {
+                    seats[i].price = 8000;
+                }
+                // Khi chọn chỗ ngồi, ẩn thanh thành tiền đi
+                seats[i].button.Click += (s, e) => HideTotalCost();
+
+            }
         }
 
+        // Ẩn thanh thành tiền
+        void HideTotalCost()
+        {
+            panelThanhTien.Visible = false;
+        }
+
+        // Nút chọn
         private void buttonChoose_Click(object sender, EventArgs e)
         {
-
+            int total = 0;
+            foreach( Seat seat in seats)
+            {
+                if (seat is null) continue;
+                total += seat.Order();
+            }
+            panelThanhTien.Visible = true;
+            textBoxCost.Text = total.ToString();
         }
 
+        // Nút hủy
         private void buttonCancel_Click(object sender, EventArgs e)
         {
-
+            foreach (Seat seat in seats)
+            {
+                if (seat is null) continue;
+                seat.Cancel();
+            }
+            panelThanhTien.Visible = true;
+            textBoxCost.Text = "0";
         }
 
+        // Nút kết thúc
         private void buttonEnd_Click(object sender, EventArgs e)
         {
-
+            Application.Exit();
         }
 
+        // Các nút chọn chỗ ngồi
         private void button1_Click(object sender, EventArgs e)
         {
             seats[1].Choose();
